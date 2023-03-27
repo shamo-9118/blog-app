c816bf3336fe34a15df5accf71cbadaf95ec251a
shamo-9118 <won9118391183@gmail.com>
2023-03-28 07:49:14 +0900

    notionからデータを取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { useCallback, useState } from 'react'
+import { getAllPosts } from '../../lib/notionApi'
 import Text from '../components/Text'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const getStaticProps = async () => {
+  const allPosts = await getAllPosts()
+  return {
+    allPosts,
+  }
+}
+
+export default function Home({ allPosts }: any) {
+  console.log(allPosts)
   return <Text text={'text'}></Text>
 }
