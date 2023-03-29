3ed765720d0f2e1ffc8a2ba6ea9ba1962ae3eb36
shamo-9118 <won9118391183@gmail.com>
2023-03-30 07:26:20 +0900

    取得できたーーーーー！！ページのidはっつけてた、、腹立たしい、、、

--- lib/notionApi.ts
+++ lib/notionApi.ts
@@ -5,15 +5,14 @@ const notion = new Client({
 })
 
 export const getAllPosts = async () => {
-  if (!process.env.NOTION_DATABASE_ID) {
-    return
-  }
+  if (!process.env.NOTION_DATABASE_ID) return
+
   const posts = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID,
     page_size: 10,
   })
+  console.log(posts)
 
   const allPosts = posts.results
   return allPosts
 }
-getAllPosts()
