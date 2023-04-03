2d8ec2e3990a38b87185b86a8dfa009f5f154e99
shamo-9118 <won9118391183@gmail.com>
2023-04-04 07:26:03 +0900

    any地獄だけど

--- lib/notionApi.ts
+++ lib/notionApi.ts
@@ -1,40 +1,13 @@
 import { Client } from '@notionhq/client'
 
-type Post = {
-  id: any
-}
-
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
 export const getAllPosts = async () => {
   if (!process.env.NOTION_DATABASE_ID) return
-
   const posts = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID,
-    page_size: 10,
   })
-
-  const allPosts = posts.results
-  allPosts.map((post) => {
-    // console.log(post.properties.タイトル)
-    getAllPageMetaData(post)
-  })
-  return allPosts
+  return posts.results
 }
-
-const getAllPageMetaData = (post: any) => {
-  return {
-    // id: post.id,
-    // title: post.properties.タイトル.title[0].plain_text,
-    // description: post.properties.Description.rich_text[0].plain_text,
-    // date: post.properties.Date.date.start,
-    // slug: post.properties.Slug.rich_text[0].plain_text,
-  }
-}
-// id,
-// title,
-// description
-// data
-// slug
