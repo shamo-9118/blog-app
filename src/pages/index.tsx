0c6c5993d997fb5ac57f1ad65a6e69f6b5df7433
shamo-9118 <won9118391183@gmail.com>
2023-04-03 22:48:42 +0900

    タグを取得してデータに追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,14 +23,22 @@ export const getStaticProps = async () => {
 }
 
 export default function Home({ allPosts }: any) {
-  const posts = allPosts.map((post: any) => {
+  const postsData = allPosts.map((post: any) => {
+    const getTags = (tags: any) => {
+      const allTags = tags.map((tag: any) => {
+        return tag.name
+      })
+      return allTags
+    }
     return {
       id: post.id,
       date: post.properties.投稿日.date.start,
       slug: post.properties.スラグ.rich_text[0].plain_text,
+      tag: getTags(post.properties.タグ.multi_select),
       title: post.properties.タイトル.title[0].plain_text,
       text: post.properties.テキスト.rich_text[0].plain_text,
     }
   })
+
   return <Text text={'text'}></Text>
 }
